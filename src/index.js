ce70d13f3e82c2e6c59052474a7e42d31e750cb9
Jam Risser <jam@jamrizzi.com>
2018-01-16 00:54:38 -0600

    Added click events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import puppeteer from 'puppeteer';
 import _ from 'lodash';
 import yaml from 'js-yaml';
@@ -24,17 +26,67 @@ new Promise((resolve, reject) => {
     const page = await browser.newPage();
     page.on('console', (log) => console[log._type](log._text));
     await page.goto(pageInfo.url);
-    await page.addScriptTag({
-      path: path.resolve(__dirname, '../node_modules/popform/umd/popform.min.js')
-    })
-    const boo = await page.evaluate((pageInfo) => {
-      return window.popform(pageInfo.config);
-    }, pageInfo);
-    console.log(`Populated ${pageInfo.name}: ${pageInfo.url}`);
-    if (commander.debug) await page.screenshot({ path: `${pageInfo.name}.debug.png` });
+    if (_.isArray(pageInfo.config)) {
+      let count = 0;
+      for (const config of pageInfo.config) {
+        await popform({
+          name: `${pageInfo.name}-${count}`,
+          url: pageInfo.url,
+          delay: pageInfo.delay,
+          page,
+          config
+        });
+        count++;
+      }
+    } else {
+      await popform({
+        name: pageInfo.name,
+        url: pageInfo.url,
+        delay: pageInfo.delay,
+        page,
+        config: pageInfo.config
+      });
+    }
   });
   await Promise.all(promises);
   await browser.close();
 }).catch((err) => {
   console.error(err);
 });
+
+async function popform({ name, url, delay, page, config }) {
+  const _config = _.clone(config);
+  delete _config.submit;
+  delete _config.keys;
+  delete _config.click;
+  await page.addScriptTag({
+    path: path.resolve(__dirname, '../node_modules/popform/umd/popform.min.js')
+  })
+  await page.evaluate((config) => {
+    return window.popform(config);
+  }, _config);
+  if (config.click) {
+    for (const query of config.click) {
+      await Promise.all([
+        page.click(query),
+        page.waitForNavigation({
+          timeout: config.delay || 10000
+        }).catch(err => true)
+      ]);
+    }
+  }
+  if (config.keys) {
+    for (const key of config.keys) {
+      await Promise.all([
+        page.keyboard.press(key),
+        page.waitForNavigation({
+          timeout: config.delay || 10000
+        }).catch(err => true)
+      ]);
+    }
+  }
+  console.log(`Populated ${name}: ${url}`);
+  if (commander.debug) {
+    await page.screenshot({ path: `${name}.debug.png` });
+  }
+}
